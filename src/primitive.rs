fd6d3274653c44c6cb40ffb11b873b3b2aed291d
Rune Tynan <runetynan@gmail.com>
2022-06-07 15:09:04 -0400

    Add punct and keyword parsers

--- src/primitive.rs
+++ src/primitive.rs
@@ -2,10 +2,83 @@
 
 use chumsky::prelude::*;
 use chumsky::error::Error;
-use proc_macro2::{Punct, Spacing};
+use proc_macro2::{Punct, Ident, Spacing, Span};
 
 use crate::{RustToken, RustSpan};
 use crate::utils::punct_eq;
+
+/// Accepts only an exact identifier, output `()` on success
+///
+/// # Examples
+///
+/// ```
+/// # use chumsky_proc::primitive::keyword;
+/// # use chumsky_proc::{stream_from_tokens, RustToken, RustSpan};
+/// # use chumsky::Parser;
+/// # use chumsky::primitive::filter_map;
+/// # use chumsky::error::Cheap;
+/// # use quote::quote;
+/// let parser = keyword::<Cheap<_, RustSpan>>("struct")
+///     .ignore_then(filter_map(RustToken::filter_ident));
+///
+/// parser.parse(stream_from_tokens(quote!(struct Foo)))
+///     .unwrap();
+///
+/// parser.parse(stream_from_tokens(quote!(enum Foo)))
+///     .unwrap_err();
+/// ```
+pub fn keyword<'a, E: 'a + Error<RustToken, Span = RustSpan>>(keyword: &'a str) -> impl Parser<RustToken, (), Error = E> + Clone + 'a {
+    filter_map(move |span, tok: RustToken| {
+        tok.into_ident()
+            .and_then(|ident| if ident == keyword {
+                Ok(())
+            } else {
+                Err(RustToken::Ident(ident))
+            })
+            .map_err(|tok| E::expected_input_found(
+                span,
+                [Some(RustToken::Ident(Ident::new(keyword, Span::mixed_site())))],
+                Some(tok),
+            ))
+    })
+}
+
+/// Accepts a single punctuation token, joined or not
+///
+/// # Examples
+///
+/// ```
+/// # use chumsky_proc::primitive::punct;
+/// # use chumsky_proc::{stream_from_tokens, RustToken, RustSpan};
+/// # use chumsky::Parser;
+/// # use chumsky::primitive::filter_map;
+/// # use chumsky::error::Cheap;
+/// # use quote::quote;
+/// let parser = filter_map::<_, _, _, Cheap<_, RustSpan>>(RustToken::filter_ident)
+///     .then_ignore(punct('+'))
+///     .then(filter_map(RustToken::filter_ident));
+///
+/// parser.parse(stream_from_tokens(quote!(a + b)))
+///     .unwrap();
+///
+/// parser.parse(stream_from_tokens(quote!(a - b)))
+///     .unwrap_err();
+/// ```
+pub fn punct<E: Error<RustToken, Span = RustSpan>>(c: char) -> impl Parser<RustToken, (), Error = E> + Clone {
+    filter_map(move |span, tok: RustToken| {
+        tok.into_punct()
+            .and_then(|punct| if punct.as_char() == c {
+                Ok(())
+            } else {
+                Err(RustToken::Punct(punct))
+            })
+            .map_err(|tok| E::expected_input_found(
+                span,
+                [Some(RustToken::Punct(Punct::new(c, Spacing::Alone)))],
+                Some(tok),
+            ))
+    })
+}
 
 /// Generate a parser for a series of joined punct tokens, with the ending allowing any spacing.
 /// Given `"+="`, this will match `+=` and `+=+`, but not `+ =`.
@@ -14,11 +87,11 @@ use crate::utils::punct_eq;
 ///
 /// ```
 /// # use chumsky_proc::primitive::joined_punct;
-/// # use chumsky_proc::{stream_from_tokens, RustToken, RustSpan};
-/// # use proc_macro2::{Punct, Spacing};
+/// # use chumsky_proc::{stream_from_tokens, RustSpan};
 /// # use chumsky::Parser;
+/// # use chumsky::error::Cheap;
 /// # use quote::quote;
-/// let parser = joined_punct::<chumsky::error::Cheap<_, RustSpan>>("+=");
+/// let parser = joined_punct::<Cheap<_, RustSpan>>("+=");
 ///
 /// parser.parse(stream_from_tokens(quote!(+=))).unwrap();
 ///
@@ -27,7 +100,7 @@ use crate::utils::punct_eq;
 /// parser.parse(stream_from_tokens(quote!(+ =))).unwrap_err();
 /// ```
 ///
-pub fn joined_punct<E: Error<RustToken, Span = RustSpan>>(punct: &str) -> impl Parser<RustToken, Vec<Punct>, Error = E> {
+pub fn joined_punct<E: Error<RustToken, Span = RustSpan>>(punct: &str) -> impl Parser<RustToken, Vec<Punct>, Error = E> + Clone {
     use chumsky::prelude::*;
 
     if punct.is_empty() {
